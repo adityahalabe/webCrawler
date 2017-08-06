73adac6b18afccb0f25301ce462baafd2292f1dd
Halabe <halbeaditya@gmail.com>
2017-08-06 16:09:25 +0530

    Aditya - Consolidating links and maintaining actors count

--- app/wipro/crawler/Crawler.scala
+++ app/wipro/crawler/Crawler.scala
@@ -4,6 +4,9 @@ import akka.actor.{Actor, Props}
 
 class Crawler (baseUrl : String,maxDepth : Int) extends Actor{
 
+  var totalActorsCreated = 1
+  var totalCompleted = 0
+  var crawledData = scala.collection.mutable.Map[String, Seq[String]]()
   def receive = {
     case "start" => {
       val linkExtractor = new LinkExtractor()
@@ -11,13 +14,26 @@ class Crawler (baseUrl : String,maxDepth : Int) extends Actor{
       self ! (baseUrl,links.filter(_ != baseUrl))
       linkExtractor.filterLinks(links,baseUrl) foreach {
         link => context.actorOf(Props[InternalLinkCrawler](new InternalLinkCrawler(baseUrl,link,maxDepth)))
+          totalActorsCreated += 1
       }
+      println(s"-------------Total Actors created for task: $totalActorsCreated")
+
     }
     case  (root : String,links : Seq[String]) => {
-      println("----------------------------------------------------------------------------------------------------------")
-      println(s"## Link --> $root : Total links originating from this link : " + links.length)
-      println(links.mkString("\n"))
-      println("----------------------------------------------------------------------------------------------------------")
+      crawledData += (root -> links)
+      totalCompleted += 1
+      println(s"-------------Completed for URL : $root \t\t\t||  Completed Actor count : $totalCompleted")
+      if(totalActorsCreated == totalCompleted){
+        self ! "done"
+      }
+
+    }
+    case "done" => {
+      for ((root,links) <- crawledData){
+        println(s"## Link --> $root : Total links originating from this link : " + links.length)
+        println(links.mkString("\n"))
+        println("----------------------------------------------------------------------------------------------------------")
+      }
     }
   }
 }
